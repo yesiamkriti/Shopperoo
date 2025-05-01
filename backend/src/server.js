06989cc854c8136744b42c4fcaa61eaaae18a20b
yesiamkriti <kritijulia@gmai.com>
2025-05-01 15:20:11 +0530

    updatation before creating new branch

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
-const { app } = require(".");
-const { connectDb } = require("./config/db");
-require("dotenv").config();
+const { app } = require('.')
+const { connectDb } = require('./config/db')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 8080;;
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, async () => {
-    await connectDb();
-    console.log("ecommerce API listening on port", PORT);
-});
+  await connectDb()
+  console.log('ecommerce API listening on port', PORT)
+  console.log('MongoDB connected successfully')
+})
